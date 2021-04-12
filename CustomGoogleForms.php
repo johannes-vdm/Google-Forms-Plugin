6012a83d43df1155dce144a8771eccd811bd3b51
Johannes <Johannesmerwede@gmail.com>
2021-04-12 02:00:08 +0200

    Created the availability for multiple shortcodes from database.

--- CustomGoogleForms.php
+++ CustomGoogleForms.php
@@ -11,6 +11,9 @@
  * Text Domain: CustomGoogleForms-Plugin
  */
 
+
+defined('ABSPATH') or die('No access.');
+
 require_once(plugin_dir_path(__FILE__) . 'CustomShortcode.php');
 require_once(plugin_dir_path(__FILE__) . 'GoogleFormsTable.php');
 on_activate();
@@ -30,10 +33,9 @@ function custom_google_forms()
 add_action('admin_menu', 'custom_google_forms');
 
 
-
 function google_forms_redirect()
 {
-    echo '<div class="wrap">Google Forms Customizer</div>';
+    //check form name for uniqueness.
     echo '<form method="POST"> 
     <div>
     <label>Name your form:
@@ -64,7 +66,7 @@ function google_forms_redirect()
 
     if (isset($_POST["googleFormConverted"])) {
 
-        print_r($_POST);
+        //print_r($_POST);
 
         global $wpdb;
 
@@ -83,64 +85,196 @@ function google_forms_redirect()
 }
 
 /*
-<form action="https://docs.google.com/forms/d/e/1FAIpQLSfgHydE9zOpVm39S7mG_Bh6O3fjZnJ58i2DfFz8vcBYYxoQIw/formResponse"
-    target="_self"
-    id="bootstrapForm"
-    method="POST">
-  <fieldset>
-      <h2>Contact Information<br><small>fill</small></h2>
-  </fieldset>
+<html>
+<form action="https://docs.google.com/forms/d/e/1FAIpQLSdjeZagH9IK7VqMNZR3dkX-DBQmK3XAfj42rzDWxMmiKpHzSw/formResponse" target="_self" id="bootstrapForm" method="POST">
+    <fieldset>
+        <h2>Party Invite<br><small>Test</small></h2>
+    </fieldset>
 
 
-  <!-- Field type: "short" id: "1633920210" -->
-  <fieldset>
-      <legend for="1633920210">Name</legend>
-      <div class="form-group">
-          <input id="2005620554" type="text" name="entry.2005620554" class="form-control" required>
-      </div>
-  </fieldset>
+    <!-- Field type: "choices" id: "1888537516" -->
+    <fieldset>
+        <legend for="1888537516"></legend>
+        <div class="form-group">
+            <div class="radio">
+                <label>
+                    <input type="radio" name="entry.399460587" value="Option 1">
+                    Option 1
+                </label>
+            </div>
+        </div>
+    </fieldset>
 
 
-  <!-- Field type: "short" id: "227649005" -->
-  <fieldset>
-      <legend for="227649005">Email</legend>
-      <div class="form-group">
-          <input id="1045781291" type="text" name="entry.1045781291" class="form-control" required>
-      </div>
-  </fieldset>
+    <!-- Field type: "short" id: "360421792" -->
+    <fieldset>
+        <legend for="360421792">What is your name?</legend>
+        <div class="form-group">
+            <input id="559352220" type="text" name="entry.559352220" class="form-control">
+        </div>
+    </fieldset>
 
 
-  <!-- Field type: "paragraph" id: "790080973" -->
-  <fieldset>
-      <legend for="790080973">Address</legend>
-      <div class="form-group">
-          <textarea id="1065046570" name="entry.1065046570" class="form-control" required></textarea>
-      </div>
-  </fieldset>
+    <!-- Field type: "choices" id: "1275419724" -->
+    <fieldset>
+        <legend for="1275419724">Can you attend?</legend>
+        <div class="form-group">
+            <div class="radio">
+                <label>
+                    <input type="radio" name="entry.877086558" value="Yes,  I&#x27;ll be there" required>
+                    Yes, I&#x27;ll be there
+                </label>
+            </div>
+            <div class="radio">
+                <label>
+                    <input type="radio" name="entry.877086558" value="Sorry, can&#x27;t make it" required>
+                    Sorry, can&#x27;t make it
+                </label>
+            </div>
+        </div>
+    </fieldset>
 
 
-  <!-- Field type: "short" id: "1770822543" -->
-  <fieldset>
-      <legend for="1770822543">Phone number</legend>
-      <div class="form-group">
-          <input id="1166974658" type="text" name="entry.1166974658" class="form-control" >
-      </div>
-  </fieldset>
+    <!-- Field type: "short" id: "2124567779" -->
+    <fieldset>
+        <legend for="2124567779">How many of you are attending?</legend>
+        <div class="form-group">
+            <input id="924523986" type="text" name="entry.924523986" class="form-control" required>
+        </div>
+    </fieldset>
 
 
-  <!-- Field type: "paragraph" id: "1846923513" -->
-  <fieldset>
-      <legend for="1846923513">Comments</legend>
-      <div class="form-group">
-          <textarea id="839337160" name="entry.839337160" class="form-control" ></textarea>
-      </div>
-  </fieldset>
+    <!-- Field type: "checkboxes" id: "234994015" -->
+    <fieldset>
+        <legend for="234994015">What will you be bringing?</legend>
+        <div class="form-group">
+            <p class="help-block">Let us know what kind of dish(es) you&#x27;ll be bringing</p>
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox" name="entry.186230675" value="Mains">
+                    Mains
+                </label>
+            </div>
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox" name="entry.186230675" value="Salad">
+                    Salad
+                </label>
+            </div>
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox" name="entry.186230675" value="Dessert">
+                    Dessert
+                </label>
+            </div>
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox" name="entry.186230675" value="Drinks">
+                    Drinks
+                </label>
+            </div>
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox" name="entry.186230675" value="Sides/Appetizers">
+                    Sides/Appetizers
+                </label>
+            </div>
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox" name="entry.186230675" value="__other_option__">
+                </label>
+                <input type="text" name="entry.186230675.other_option_response" placeholder="custom value">
+            </div>
+        </div>
+    </fieldset>
 
-  <input type="hidden" name="fvv" value="1">
-  <input type="hidden" name="fbzx" value="5620262948632809141">
 
-  <input type="hidden" name="pageHistory" value="0">
+    <!-- Field type: "short" id: "120135225" -->
+    <fieldset>
+        <legend for="120135225">Do you have any allergies or dietary restrictions?</legend>
+        <div class="form-group">
+            <input id="1751303409" type="text" name="entry.1751303409" class="form-control">
+        </div>
+    </fieldset>
 
-  <input class="btn btn-primary" type="submit" value="Submit">
-</form>;
+
+    <!-- Field type: "short" id: "1930256188" -->
+    <fieldset>
+        <legend for="1930256188">What is your email address?</legend>
+        <div class="form-group">
+            <input id="443565211" type="text" name="entry.443565211" class="form-control">
+        </div>
+    </fieldset>
+
+
+    <!-- Field type: "section" id: "1827333776" -->
+    <fieldset>
+        <legend for="1827333776">NEW PAGE</legend>
+        <div class="form-group">
+        </div>
+    </fieldset>
+
+
+    <!-- Field type: "linear" id: "1519072917" -->
+    <fieldset>
+        <legend for="1519072917">AGE</legend>
+        <div class="form-group">
+            <div>
+                <label class="radio-inline">
+                    <input type="radio" name="entry.509282163" value="1">
+                    1
+                </label>
+                <label class="radio-inline">
+                    <input type="radio" name="entry.509282163" value="2">
+                    2
+                </label>
+                <label class="radio-inline">
+                    <input type="radio" name="entry.509282163" value="3">
+                    3
+                </label>
+                <label class="radio-inline">
+                    <input type="radio" name="entry.509282163" value="4">
+                    4
+                </label>
+                <label class="radio-inline">
+                    <input type="radio" name="entry.509282163" value="5">
+                    5
+                </label>
+            </div>
+            <div>
+                <div>1: </div>
+                <div>5: </div>
+            </div>
+        </div>
+    </fieldset>
+
+
+    <!-- Field type: "grid" id: "987379900" -->
+    <fieldset>
+        <legend for="987379900">backlit</legend>
+        <div class="form-group">
+            <div>
+                <span>Row 1: </span>
+                <label class="radio-inline">
+                    <input type="radio" name="entry.2108237869" value="Column 1">
+                    Column 1
+                </label>
+            </div>
+        </div>
+    </fieldset>
+
+    <input type="hidden" name="fvv" value="1">
+    <input type="hidden" name="fbzx" value="4334292585893045143">
+    <!--
+        CAVEAT: In multipages (multisection) forms, *pageHistory* field tells to google what sections we've currently completed.
+        This usually starts as "0" for the first page, then "0,1" in the second page... up to "0,1,2..N" in n-th page.
+        Keep this in mind if you plan to change this code to recreate any sort of multipage-feature in your exported form.
+        We're setting this to the total number of pages in this form because we're sending all fields from all the section together.
+    -->
+    <input type="hidden" name="pageHistory" value="0,1">
+
+    <input class="btn btn-primary" type="submit" value="Submit">
+</form>
+
+</html>
 */
--- CustomShortcode.php
+++ CustomShortcode.php
@@ -1,59 +1,50 @@
 <?php
-//$table = $wpdb->prefix . "GoogleForms";
-//$sql = `SELECT * FROM $table`;
+
+
+//$FormID = 1; //INCREMENT IN FUTURE WITH FOR LOOP IF $FORMID NOT FOUND THEN $FORMID + 1;
+
+
+//global $wpdb;
+
+//$allposts = $wpdb->get_results("SELECT * FROM " . $wpdb->prefix . "GoogleForms", ARRAY_A);
+
+
+
+
+
+
+if (!defined('ABSPATH'))
+    exit;
 
 global $wpdb;
 
-$allposts = $wpdb->get_results("SELECT * FROM wp_GoogleForms", ARRAY_A);
-//print_r($allposts);
 
-
-$last = count($allposts) - 1;
-foreach ($allposts as $i => $row) {
-    $isFirst = ($i == 0);
-    $isLast = ($i == $last);
-
-    $formID = $row['formID'];
-
-    add_shortcode(('GoogleForm' . $formID), 'sample_shortcode');
-}
-
-function sample_shortcode()
+add_shortcode('GoogleForm', 'GoogleForm_display_content');
+//[GoogleForm snippet="1"]
+function GoogleForm_display_content($shortcode_class)
 {
+
     global $wpdb;
 
-    $allposts = $wpdb->get_results("SELECT * FROM wp_GoogleForms", ARRAY_A);
+    //if (is_array($shortcode_class) && isset($shortcode_class['snippet'])) {
 
-    $last = count($allposts) - 1;
-    foreach ($allposts as $i => $row) {
-        $isFirst = ($i == 0);
-        $isLast = ($i == $last);
+    $shortcode_name = $shortcode_class['snippet'];
 
-        return $row['convertedFormHTML'];
-        $formID = $row['formID'];
-        //DISPLAY ALL DATA FROM ROW:    // print_r($row); 
 
-        add_shortcode(('GoogleForm' . $formID), 'sample_shortcode');
+    //$query = $wpdb->get_results($wpdb->prepare("SELECT * FROM " . $wpdb->prefix . "GoogleForms WHERE formName=%s", $shortcode_name));
+
+
+    $query = "SELECT * FROM " . $wpdb->prefix . "GoogleForms WHERE '$shortcode_name' = formName";
+    //$initialize = $wpdb->get_results($wpdb->prepare("SELECT * FROM " . $wpdb->prefix . "GoogleForms WHERE //formName=%s", $shortcode_name));
+
+    $result = $wpdb->get_results($query);
+
+    global $wpdb;
+
+
+    foreach ($result as $values) {
+
+        $htmlConverted = $values->convertedFormHTML;
+        return $htmlConverted;
     }
-
-    //return "HELLO TEST ME";
 }
-
-//CHANGE GoogleForm to unique name supplied by user SELECTED FROM DATABASE
-
-echo "<pre>[<code>sample_shortcode</code>]</pre>";
-
-
-
-/*
-function sample_shortcode()
-{
-    return "HELLO TEST ME";
-}
-
-//CHANGE GoogleForm to unique name supplied by user SELECTED FROM DATABASE
-add_shortcode('GoogleForm', 'sample_shortcode');
-echo "<pre>[<code>sample_shortcode</code>]</pre>";
-
-
-*/
