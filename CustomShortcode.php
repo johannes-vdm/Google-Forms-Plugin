28a448242d89de38d46cb343c19d2ef157653a09
Johannes <Johannesmerwede@gmail.com>
2021-04-14 22:12:01 +0200

    -Created Basic Timer -Opened time variable to timer.js from database. -Created timer.css

--- CustomShortcode.php
+++ CustomShortcode.php
@@ -1,27 +1,17 @@
 <?php
-//$FormID = 1; //INCREMENT IN FUTURE WITH FOR LOOP IF $FORMID NOT FOUND THEN $FORMID + 1;
-//global $wpdb;
-//$allposts = $wpdb->get_results("SELECT * FROM " . $wpdb->prefix . "GoogleForms", ARRAY_A);
 
 if (!defined('ABSPATH'))
   exit;
 
 global $wpdb;
 
-
-
-
 add_action('wp_enqueue_scripts', 'load_ss');
-
-
-
 
 function load_ss()
 {
   wp_enqueue_script(
     'CustomGoogleFormTimer',
     plugin_dir_url(__FILE__) . 'js/timer.js',
-    array('jquery'),
     '1.0',
     false
   );
@@ -29,8 +19,6 @@ function load_ss()
 
 
 add_shortcode('GoogleForm', 'GoogleForm_display_content');
-
-
 
 function GoogleForm_display_content($shortcode_class)
 {
@@ -48,22 +36,7 @@ function GoogleForm_display_content($shortcode_class)
     $htmlConverted = $values->convertedFormHTML;
     $seconds = $values->timer;
 
-    /*  $jsHTML = '<div class="container">
-    <form class="form">
- 
-      <input type="number" min="1" class="time-input" placeholder="Enter Countdown">
-   
-      <button type="submit" class="set-btn">SET</button>
-    </form>
-    <p class="countdown">00 : 00 : 00</p>
-    <div class="buttons">
-      <button class="stop-btn" disabled>STOP</button>
-      <button class="reset-btn" disabled>RESET</button>
-    </div>
-  </div>';*/
-    // echo "CHARLES";
-    //echo "HERE ARE THE SECONDS" . $seconds;
-    $jsHTML = '<div><h1>OVER HERE</h1></div><div class="CountdownTime" data-value=' . $seconds . '></div>';
+    $jsHTML = '<div><h4 class="CountdownTime" value=' . $seconds . '>00:00:00</h4></div>';
 
     return $htmlConverted . $jsHTML;
   }
--- js/timer.js
+++ js/timer.js
@@ -1,14 +1,32 @@
 window.onload = function () {
-    var classValue = document.getElementsByClassName("CountdownTime");
 
-    console.log("LENGTH: " + classValue.length);
+    const MultipleCountdownElements = document.getElementsByClassName('CountdownTime');
 
-    var secondsHTML;
+    console.log(MultipleCountdownElements.length);
 
-    for (var i = 0; i < classValue.length; i++) {
-        secondsHTML = classValue[i].getAttribute('data-value');
+    //dont enter with no classes
+    if (MultipleCountdownElements.length > 0) {
 
-        console.log("data-value: " + secondsHTML);
+        CountdownElements = MultipleCountdownElements[0];
+
+        console.log(CountdownElements);
+
+        let time = CountdownElements.getAttribute('value');
+
+        setInterval(updateCountdown, 1000);
+
+        function updateCountdown() {
+            const minutes = Math.floor(time / 60);
+            let seconds = time % 60;
+
+            CountdownElements.innerHTML = `${minutes}:${seconds}`;
+            time--;
+        }
+
+        if (time < 0) {
+            getElementsByClassName('btn-primary')
+
+        }
     }
 
 }
