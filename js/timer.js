001e6bcaa4b2d7d8fbea7d878fa794feff8f6198
Johannes <Johannesmerwede@gmail.com>
2021-04-15 23:14:15 +0200

    optional hours to timer with splicing

--- js/timer.js
+++ js/timer.js
@@ -1,31 +1,36 @@
 window.onload = function () {
 
     const MultipleCountdownElements = document.getElementsByClassName('CountdownTime');
+    CountdownElements = MultipleCountdownElements[0];
+    let time = CountdownElements.getAttribute('value');
+    setInterval(updateCountdown, 1000);
 
-    console.log(MultipleCountdownElements.length);
-
-    //dont enter with no classes
-    if (MultipleCountdownElements.length > 0) {
-
-        CountdownElements = MultipleCountdownElements[0];
-
-        console.log(CountdownElements);
-
-        let time = CountdownElements.getAttribute('value');
-
-        setInterval(updateCountdown, 1000);
-
-        function updateCountdown() {
-            const minutes = Math.floor(time / 60);
+    function updateCountdown() {
+        if (time < 3600) {
+            let minutes = Math.floor(time / 60);
             let seconds = time % 60;
 
+            seconds = ('0' + seconds).slice(-2);
+            minutes = ('0' + minutes).slice(-2);
+
             CountdownElements.innerHTML = `${minutes}:${seconds}`;
+            time--;
+
+        } else {
+            let hours = Math.floor(time / 3600);
+            let minutes = Math.floor(time / 60 - 60);
+            let seconds = time % 60;
+
+            seconds = ('0' + seconds).slice(-2);
+            minutes = ('0' + minutes).slice(-2);
+            hours = ('0' + hours).slice(-2);
+
+            CountdownElements.innerHTML = `${hours}:${minutes}:${seconds}`;
             time--;
         }
 
         if (time < 0) {
-            getElementsByClassName('btn-primary')
-
+            document.getElementById('btn-primary').submit();
         }
     }
 
