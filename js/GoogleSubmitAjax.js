fa691f56c9628c068bd2d82776d94610c582721e
Johannes <Johannesmerwede@gmail.com>
2021-04-22 23:29:47 +0200

    added default ajax from conversion website

--- js/GoogleSubmitAjax.js
+++ js/GoogleSubmitAjax.js
@@ -1,42 +1,15 @@
-
-jQuery('#ReadyBtn').submit(function () {
-    alert("YES");
-    e.preventDefault();
-
-});
-
-
-//Unresponsive
-//COPIED FROM https://stefano.brilli.me/google-forms-html-exporter/
-//This should prevent a user from entering the google docs url after the form is submitted to stop users from changing the form after it is submitted(with or without timer.js). 
-//Connected to CustomShortcode.php
-
-//jQuery('#bootstrapForm').submit(function (event) {
-//    //alert("YES"); 
-//    event.preventDefault()
-//    var extraData = {}
-//    jQuery('#bootstrapForm').ajaxSubmit({
-//        data: extraData,
-//        dataType: 'jsonp',
-//        error: function () {
-//
-//            alert('Form Submitted. Thanks.')
-//
-//        }
-//    });
-//});
-
-//ATTEMPT TO CONVERT JQUERY TO VANILLA JAVASCRIPT
-/*
-document.querySelector('#bootstrapForm').submit(function (event) {
+jQuery('#bootstrapForm').submit(function (event) {
     event.preventDefault()
     var extraData = {}
-    document.querySelector('#bootstrapForm').fetch({
+    $('#bootstrapForm').ajaxSubmit({
         data: extraData,
-        dataType: 'jsonp',
+        dataType: 'jsonp',  // This won't really work. It's just to use a GET instead of a POST to allow cookies from different domain.
         error: function () {
+            // Submit of form should be successful but JSONP callback will fail because Google Forms
+            // does not support it, so this is handled as a failure.
             alert('Form Submitted. Thanks.')
+            // You can also redirect the user to a custom thank-you page:
+            // window.location = 'http://www.mydomain.com/thankyoupage.html'
         }
     })
 })
-*/
